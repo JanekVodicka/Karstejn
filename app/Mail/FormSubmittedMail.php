d6563e1d1c9d62536351c21f3e091ec7cf59e2f9
Janek <vod.janek@gmail.com>
2024-11-22 17:40:43 +0100

    Edit FormSubmittedMail

--- app/Mail/FormSubmittedMail.php
+++ app/Mail/FormSubmittedMail.php
@@ -2,23 +2,30 @@
 
 namespace App\Mail;
 
+use App\Models\FormModel;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Attachment;
 
 class FormSubmittedMail extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public FormModel $form;
+    public $rocnik;
+    protected array $attachmentPaths; // Array of file paths for attachments
     /**
      * Create a new message instance.
      */
-    public function __construct()
+    public function __construct($formData, $attachmentPaths, $rocnik)
     {
-        //
+        $this->form = $formData;
+        $this->rocnik = $rocnik;
+        $this->attachmentPaths = $attachmentPaths;
     }
 
     /**
@@ -38,6 +45,10 @@ class FormSubmittedMail extends Mailable
     {
         return new Content(
             view: 'view.name',
+            with: [
+                'form' => $this->form,
+                'rocnik' => $this->rocnik,
+            ]
         );
     }
 
@@ -48,6 +59,13 @@ class FormSubmittedMail extends Mailable
      */
     public function attachments(): array
     {
-        return [];
+        return collect($this->attachmentPaths)->map(function ($path) {
+            return Attachment::fromPath($path)
+                             ->as(basename($path)) // Use the file's base name for attachment
+                             ->withMime('application/pdf'); // Adjust MIME type if necessary
+        })->toArray();
+    }
+    public function build(){
+        return $this->markdown('emails.prihlaska_email');
     }
 }
