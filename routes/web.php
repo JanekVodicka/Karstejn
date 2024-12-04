418037ed9e69660c0f45c2c81b1b27e7f641c0b3
Janek <vod.janek@gmail.com>
2024-12-04 21:45:15 +0100

    app.css + web.php update

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\GalleryController;
 use App\Http\Controllers\FormController;
 use App\Http\Controllers\DbViewController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\InfoController;
 
 
 Route::get('/', function () {
@@ -18,17 +21,16 @@ Route::get('galerie_ostatni', function () {
     return view('galerie_ostatni');
 })->name('galerie_ostatni');
 
-Route::get('galerie_rocniky', function() {
-    return view('galerie_rocniky');
-})->name('galerie_rocniky');
+Route::get('galerie_rocniky', [GalleryController::class, 'showToGalerieRocniky'])
+->name('galerie_rocniky');
 
 Route::get('galerie_videa', function() {
     return view('galerie_videa');
 })->name('galerie_videa');
 
-Route::get('info', function() {
-    return view('info');
-})->name('info');
+Route::get('info',[InfoController::class, 'showToInfo'])
+->name('info');
+
 
 Route::get('kronika', function() {
     return view('kronika');
@@ -38,19 +40,29 @@ Route::get('prihlaska', function() {
     return view('prihlaska');
 })->name('prihlaska');
 
-Route::get('settings', function() {
-    return view('settings');
-})->name('settings');
+Route::get('login', function(){
+    return view('login');
+})->name('login');
 
-// Route::get('prihlasky-private', function() {
-//     return view('prihlasky-private');
-// })->name('prihlasky-private');
+Route::post('login', [LoginController::class, 'login'])->name('login.form');
 
-Route::get('prihlasky-private', [DbViewController::class, 'showPrihlaskyDb'])
-    ->name('prihlasky-private');
-
-Route::get('galerie/{event}/{year}', [GalleryController::class, 'show'])
+Route::get('galerie/{event}/{year}', [GalleryController::class, 'showToGalerie'])
     ->name('galerie');
 
 Route::post('form', [FormController::class, 'handleFormSubmission'])
 ->name('prihlaska.store');
+ 
+// Route accessible only by admin
+Route::get('prihlasky-private', [DbViewController::class, 'showPrihlaskyDb'])
+    ->middleware('admin')  // Use 'admin' middleware here
+    ->name('prihlasky-private');
+ 
+// Route accessible by all auth
+Route::get('/settings', function () {
+    return view('settings');
+})->name('settings')->middleware('auth'); // Use 'auth' middleware for all users
+
+Route::post('logout', function () {
+    Auth::logout();
+    return redirect()->route('login');
+})->name('logout');
