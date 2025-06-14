a4456ff0c490e0171253d6f6b544618f8a0ddfb8
Janek <vod.janek@gmail.com>
2025-06-14 20:51:00 +0200

    test branch

--- resources/views/galerie_akce.blade.php
+++ resources/views/galerie_akce.blade.php
@@ -9,7 +9,7 @@
                     <div class="album" style="background-image: url(images/akce/{{ $album['termin_akce'] }}.jpg);">
                         <a href="{{ url("galerie/akce/{$album['termin_akce']}") }}">
                             <div class="img-text">{{ $album['termin_akce'] }}</div>
-                            <div class="img-text-hidden">{{ $album['tema_akce'] }}</div> 
+                            <div class="img-text-hidden">{{ $album['tema_akce'] }}</div>
                         </a>
                     </div>
                 @endforeach
